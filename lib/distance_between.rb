64cfa76d39fdf08a5bbd61a2989102ff7c34ba39
Chad Campbell <ccampb13@gmail.com>
2014-07-22 10:19:49 -0500

    vector attempted

---
+++ lib/distance_between.rb
@@ -0,0 +1,14 @@
+require 'matrix'
+
+class DistanceBetween < Vector
+
+  def initialize(x,y)
+    @x = x
+    @y = y
+  end
+
+  def distance
+    
+  end
+
+end
---
+++ test/test_distance_between.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+require 'matrix'
+require_relative '../lib/distance_between'
+
+class DistanceBetween < MiniTest::Unit::TestCase
+
+  def setup
+    # This is ran before all the tests. Useful
+    # when preparing input data for the objects/methods you
+    # wish to test.
+  end
+
+end
